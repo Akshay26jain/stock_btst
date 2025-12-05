694e377ebe54c7855a8a1106061793cc37992209
Akshay Jain <akshayjain@Akshays-MacBook-Air.local>
2025-12-05 22:36:37 +0530

    adding code

--- script.py
+++ script.py
@@ -1,5 +1,18 @@
 import datetime
+import requests
 
+SEND_TELEGRAM = True  # Set True if you want alerts
+BOT_TOKEN = "8578835344:AAHFnHTgi6S4hzhB0C-e_iwQFw302e-G2v8"
+CHAT_ID = "933977274"
+
+def send_telegram(msg):
+    if SEND_TELEGRAM:
+        requests.get(
+            f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+            params={"chat_id": CHAT_ID, "text": msg}
+        )
+
+send_telegram("Akshay")
 
 def main():
     print("Running scheduled job at:", datetime.datetime.utcnow())
@@ -7,3 +20,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    send_telegram("Test from github")
